e0a96ddd41455434303edc66d67246801d572082
Milouyg <33333@ma-web.nl>
2022-11-05 14:39:50 +0100

    js cursus works

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
+// const buttonPython = document.getElementById("python");
+// const buttonExcel = document.getElementById("excel");
 
+// buttonPython.onclick = function () {
+//     var x = document.getElementsByClassName("cursus__p--python")[0];
+//     x.style.display = "block";
+//     var y = document.getElementsByClassName("cursus__p--excel")[0];
+//     y.style.display = "none";
+// }
 
-const buttonPython = document.getElementById("python");
-const buttonExcel = document.getElementById("excel");
+// buttonExcel.onclick = function () {
+//     var x = document.getElementsByClassName("cursus__p--python")[0];
+//     x.style.display = "none";
+//     var y = document.getElementsByClassName("cursus__p--excel")[0];
+//     y.style.display = "block";
+// }
 
-buttonPython.onclick = function () {
-    var x = document.getElementsByClassName("cursus__p--python")[0];
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+// Remove and add text from curses
+var buttons = document.getElementsByClassName("listItem__btn");
+var paragraphs = document.querySelectorAll(".cursus__figure > .cursus__p");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        for (let j = 0; j < paragraphs.length; j++) {
+            paragraphs[j].style.display = "none";
+        }
+        paragraphs[i].style.display = "block";
     }
 }
-
-buttonExcel.onclick = function () {
-    var y = document.getElementsByClassName("cursus__p--excel")[0];
-    if (y.style.display === "none") {
-        y.style.display = "block";
-    } else {
-        y.style.display = "none";
-    }
-}
-
-
-
-
-
