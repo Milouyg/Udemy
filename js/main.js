e571fec7496f263c04eb3ff69ea74d1dabb6cd3b
Milouyg <33333@ma-web.nl>
2022-11-08 13:41:03 +0100

    Content updated to games

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-// const buttonPython = document.getElementById("python");
-// const buttonExcel = document.getElementById("excel");
-
-// buttonPython.onclick = function () {
-//     var x = document.getElementsByClassName("cursus__p--python")[0];
-//     x.style.display = "block";
-//     var y = document.getElementsByClassName("cursus__p--excel")[0];
-//     y.style.display = "none";
-// }
-
-// buttonExcel.onclick = function () {
-//     var x = document.getElementsByClassName("cursus__p--python")[0];
-//     x.style.display = "none";
-//     var y = document.getElementsByClassName("cursus__p--excel")[0];
-//     y.style.display = "block";
-// }
-
-// Remove and add text from curses
+// Remove and add text from cursus
 var buttons = document.getElementsByClassName("listItem__btn");
 var paragraphs = document.querySelectorAll(".cursus__figure > .cursus__p");
 
